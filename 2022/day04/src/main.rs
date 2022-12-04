d965e53ee72b113fbfcb24c5b32fa1f11d20b264
mbyt <random.seed@web.de>
2022-12-04 14:55:13 +0100

    resolved day 4, part 2

--- 2022/day04/src/main.rs
+++ 2022/day04/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let contents = r#"
+    let example = r#"
         2-4,6-8
         2-3,4-5
         5-7,7-9
@@ -13,7 +13,7 @@ fn main() {
     "#
     .trim();
 
-    assert_eq!(camp_cleanup_reconsideration_count(contents), 2);
+    assert_eq!(camp_cleanup_reconsideration_count(example), 2);
 
     let path = Path::new("input");
     assert!(path.exists());
@@ -24,17 +24,31 @@ fn main() {
     println!("Total reconsideration count: {total_reconsideration_count}");
     // regression test for refactoring
     assert_eq!(total_reconsideration_count, 483);
+
+    // part 2
+    assert_eq!(camp_cleanup_overlap_count(example), 4);
+
+    let total_overlap_count = camp_cleanup_overlap_count(&contents);
+    println!("Total overlap count: {total_overlap_count}");
+    // regression test for refactoring
+    assert_eq!(total_overlap_count, 874);
+}
+
+fn camp_cleanup_overlap_count(contents: &str) -> i32 {
+    let mut total_overlap_count: i32 = 0;
+    for line_wspace in contents.lines() {
+        let (srng1, srng2) = get_range_sets_from_line(line_wspace);
+        if !srng1.is_disjoint(&srng2) {
+            total_overlap_count += 1;
+        }
+    }
+    total_overlap_count
 }
 
 fn camp_cleanup_reconsideration_count(contents: &str) -> i32 {
     let mut total_score: i32 = 0;
     for line_wspace in contents.lines() {
-        let line = line_wspace.trim();
-        let parts: Vec<&str> = line.split(",").collect();
-        assert_eq!(parts.len(), 2);
-        // range_from_part
-        let srng1 = set_from_str_range(parts[0]);
-        let srng2 = set_from_str_range(parts[1]);
+        let (srng1, srng2) = get_range_sets_from_line(line_wspace);
         if srng1.is_subset(&srng2) | srng2.is_subset(&srng1) {
             total_score += 1;
         }
@@ -42,8 +56,21 @@ fn camp_cleanup_reconsideration_count(contents: &str) -> i32 {
     total_score
 }
 
-fn set_from_str_range(line: &str) -> HashSet<i32> {
-    let start_stop: Vec<i32> = line.split('-').map(|s| s.parse::<i32>().unwrap()).collect();
+fn get_range_sets_from_line(line_wspace: &str) -> (HashSet<i32>, HashSet<i32>) {
+    let line = line_wspace.trim();
+    let parts: Vec<&str> = line.split(",").collect();
+    assert_eq!(parts.len(), 2);
+    // range_from_part
+    let srng1 = set_from_range_str(parts[0]);
+    let srng2 = set_from_range_str(parts[1]);
+    (srng1, srng2)
+}
+
+fn set_from_range_str(range_str: &str) -> HashSet<i32> {
+    let start_stop: Vec<i32> = range_str
+        .split('-')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
     assert_eq!(start_stop.len(), 2);
     let range: HashSet<i32> = (start_stop[0]..=start_stop[1]).collect();
     range
